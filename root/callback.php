70038f0465d4a9bd67cb59411c7ca0e0f0655917
tyk2013 <ken.rad0202@gmail.com>
2016-10-23 01:10:55 +0900

    ok

--- root/callback.php
+++ root/callback.php
@@ -6,6 +6,20 @@
  * Time: 0:34
  */
 
+use LINE\LINEBot\Event\BeaconDetectionEvent;
+use LINE\LINEBot\Event\FollowEvent;
+use LINE\LINEBot\Event\JoinEvent;
+use LINE\LINEBot\Event\LeaveEvent;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\AudioMessage;
+use LINE\LINEBot\Event\MessageEvent\ImageMessage;
+use LINE\LINEBot\Event\MessageEvent\LocationMessage;
+use LINE\LINEBot\Event\MessageEvent\StickerMessage;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Event\MessageEvent\VideoMessage;
+use LINE\LINEBot\Event\PostbackEvent;
+use LINE\LINEBot\Event\UnfollowEvent;
+
 define("LINE_MESSAGING_API_CHANNEL_SECRET", '7ce26fb54a53c4d258e855b7f49d0c37');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'WGI2sHZrfhY94b4HurfkPmIINGpRw47EqBz3iJHOXtnloBAnZPlo6X293XM1RIUYuGMZJFWzCTXQkUvtDs7WKh7CbmbICOcOmp4m+ets5UMbOHPDerVwO+dlTagr7EaWQRTxuzM4z/dj1+z6jSwfWgdB04t89/1O/w1cDnyilFU=ISSUE');
 
@@ -16,16 +30,48 @@ $bot = new \LINE\LINEBot(
     ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
 );
 
+//エラー処理
+if(!isset($_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE])){
+    error_log("エラーです");
+    responseBadRequest("誤ったリクエストです");
+}
+
+function responseBadRequest($reason){
+    http_response_code(400);
+    echo 'Bad request'.$reason;
+    exit;
+}
+
 $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $body = file_get_contents("php://input");
 
+
+//イベントごとに場合分け
 $events = $bot->parseEventRequest($body, $signature);
 
 foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+    if ($event instanceof TextMessage) {
         $reply_token = $event->getReplyToken();
         $text = $event->getText();
         $bot->replyText($reply_token, $text);
+    }elseif ($event instanceof StickerMessage){
+        $reply_token = $event->getReplyToken();
+        $sticker_id = $event->getStickerId();
+        $package_id = $event->getPackageId();
+
+        $sticker_builder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($package_id,$sticker_id);
+        
+        $bot->replyMessage($reply_token,$sticker_builder);
+
+
+    }elseif ($event instanceof VideoMessage){
+
+    }elseif($event instanceof LocationMessage){
+
+    }elseif($event instanceof AudioMessage){
+
+    }elseif($event instanceof ImageMessage){
+
     }
 }
 
