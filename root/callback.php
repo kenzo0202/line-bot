25ad8a1e98b0549807757bba35fb1776422cf434
tyk2013 <ken.rad0202@gmail.com>
2016-10-23 02:19:34 +0900

    ok

--- root/callback.php
+++ root/callback.php
@@ -86,6 +86,15 @@ foreach ($events as $event) {
         $bot ->replyMessage($reply_token,$video_builder);
 
     }elseif($event instanceof LocationMessage){
+        $reply_token = $event->getReplyToken();
+        $title =  "my location";
+        $address =  "〒150-0002 東京都渋谷区渋谷２丁目２１−１";
+        $latitude = 35.65910807942215;
+        $longitude = 139.70372892916203;
+        $location_builder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title,$address,$latitude,$longitude);
+
+        $bot->replyMessage($reply_token,$location_builder);
+
 
     }elseif($event instanceof AudioMessage){
 
