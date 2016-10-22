f9f01ae4d4a4bf30727eebc3a9ba1249c4f47fdc
tyk2013 <ken.rad0202@gmail.com>
2016-10-23 02:04:32 +0900

    ok

--- root/callback.php
+++ root/callback.php
@@ -78,6 +78,8 @@ foreach ($events as $event) {
             error_log($response->getHTTPStatus() . ' ' . $response->getBody());
         }
 
+        sleep(3000);
+
         $contenturl = "https://line-bot0202.herokuapp.com/video/sample.mp4";
         $imageurl = "http://hakomori.net/wp-content/uploads/yu_google1.jpg";
 
