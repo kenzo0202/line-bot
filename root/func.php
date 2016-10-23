816972d3aafc1576251e22753bf75403c75981ac
tyk2013 <ken.rad0202@gmail.com>
2016-10-23 11:01:13 +0900

    ok

--- root/func.php
+++ root/func.php
@@ -8,8 +8,15 @@
 
 //DB接続
 function db_con(){
+    $dsn = "mysql:dbname=LAA0710186-sample;host=mysql113.phy.lolipop.lan;charset=utf8";
+    $username = "LAA0710186";
+    $password = "kenrad22";
+    $driver_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
     try{
-        return new PDO('mysql:dbname=LAA0710186-sample;charset=utf8;host=mysql113.phy.lolipop.lan','LAA0710186','kenrad22');
+        return new PDO($dsn,$username,$password,$driver_options);
     }catch(PDOException $e){
 //        exit("DBconnectingerror:".$e->getMessage());
         echo $e->getMessage();
