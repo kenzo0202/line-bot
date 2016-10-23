4ab65baeac551f50fa64a86d3b755a0e97d32e86
tyk2013 <ken.rad0202@gmail.com>
2016-10-23 11:24:01 +0900

    ok

--- root/func.php
+++ root/func.php
@@ -8,15 +8,15 @@
 
 //DB接続
 function db_con(){
-    $dsn = "mysql:dbname=LAA0710186-sample;host=mysql113.phy.lolipop.lan;charset=utf8";
-    $username = "LAA0710186";
-    $password = "kenrad22";
-    $driver_options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ];
+//    $dsn = "mysql:dbname=LAA0710186-sample;host=mysql113.phy.lolipop.lan;charset=utf8";
+//    $username = "LAA0710186";
+//    $password = "kenrad22";
+//    $driver_options = [
+//        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//        PDO::ATTR_EMULATE_PREPARES => false,
+//    ];
     try{
-        return new PDO($dsn,$username,$password,$driver_options);
+        return new PDO('mysql:host=mysql113.phy.lolipop.lan;dbname=LAA0710186-sample;charset=utf8;', 'LAA0710186', 'kenrad22');
     }catch(PDOException $e){
 //        exit("DBconnectingerror:".$e->getMessage());
         echo $e->getMessage();
